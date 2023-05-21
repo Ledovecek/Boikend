5996cafb27682c3842436287822b413b12f25574
Ledovec <ledovec.official@gmail.com>
2023-05-21 22:39:07 +0200

    majority skill issues fix

--- src/test/java/me/ledovec/boikendv2/Boikendv2ApplicationTests.java
+++ src/test/java/me/ledovec/boikendv2/Boikendv2ApplicationTests.java
@@ -3,16 +3,22 @@ package me.ledovec.boikendv2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.*;
+
 @SpringBootTest
 class Boikendv2ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
         var f = new File("README.md");
-        if (f.readString() != "# Boikend
-
-<p>This repository is backend for our bouy project.</p>") {
-            throw new Exception("neser se mi do rede mecka");
+        FileReader fileReader;
+        fileReader = new FileReader(f);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String s = bufferedReader.readLine();
+        if (s != "# Boikend\n" +
+                "\n" +
+                "<p>This repository is backend for our bouy project.</p>") {
+            throw new Exception("Readme has been confused as unseemly and inconsistent with our internal conditions, which are written in the rules of the competition.");
         }
     }
 
